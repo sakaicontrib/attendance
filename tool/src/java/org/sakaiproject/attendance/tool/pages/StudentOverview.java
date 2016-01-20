6f8a1d67d9090ffeb5d40bede494cf58378aa040
Leonardo Canessa <lcanessa1@udayton.edu>
2016-01-20 15:19:05 -0500

    SAKAI-2201 StudentOverview now has data
    The StudentOverview page now shows a table of students with all their
    attendance stats.

--- tool/src/java/org/sakaiproject/attendance/tool/pages/StudentOverview.java
+++ tool/src/java/org/sakaiproject/attendance/tool/pages/StudentOverview.java
@@ -18,6 +18,17 @@ package org.sakaiproject.attendance.tool.pages;
 
 
 import org.apache.wicket.RestartResponseException;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.model.ResourceModel;
+import org.sakaiproject.attendance.model.Status;
+import org.sakaiproject.attendance.tool.dataproviders.StudentDataProvider;
+import org.sakaiproject.user.api.User;
+
+import java.util.Map;
 
 /**
  * Created by Leonardo Canessa [lcanessa1 (at) udayton (dot) edu]
@@ -31,5 +42,87 @@ public class StudentOverview extends BasePage {
         if(this.role != null && this.role.equals("Student")) {
             throw new RestartResponseException(StudentView.class);
         }
+
+        add(createHeader());
+        add(createStatsTable());
     }
+
+    private WebMarkupContainer createHeader() {
+        WebMarkupContainer contain = new WebMarkupContainer("student-overview-header");
+        Label title = new Label("student-overview-title", new ResourceModel("attendance.student.overview.title"));
+        Label subtitle = new Label("student-overview-subtitle", new ResourceModel("attendance.student.overview.subtitle"));
+
+        contain.add(title);
+        contain.add(subtitle);
+
+        return contain;
+    }
+
+    private WebMarkupContainer createStatsTable() {
+        WebMarkupContainer statsTable = new WebMarkupContainer("student-overview-stats-table");
+        statsTable.setOutputMarkupPlaceholderTag(true);
+        createStatsTableHeader(statsTable);
+        createStatsTableData(statsTable);
+
+        return statsTable;
+    }
+
+    private void createStatsTableHeader(WebMarkupContainer t) {
+        Label studentName = new Label("header-student-name", new ResourceModel("attendance.header.student"));
+        //headers for the table
+        Label headerStatusPresent 	= new Label("header-status-present", 		new ResourceModel("attendance.overview.header.status.present"));
+        Label headerStatusLate 		= new Label("header-status-late", 		new ResourceModel("attendance.overview.header.status.late"));
+        Label headerStatusLeftEarly = new Label("header-status-left-early", 	new ResourceModel("attendance.overview.header.status.left.early"));
+        Label headerStatusExcused 	= new Label("header-status-excused", 		new ResourceModel("attendance.overview.header.status.excused"));
+        Label headerStatusUnexcused = new Label("header-status-unexcused", 	new ResourceModel("attendance.overview.header.status.unexcused"));
+
+        t.add(studentName);
+        t.add(headerStatusPresent);
+        t.add(headerStatusLate);
+        t.add(headerStatusLeftEarly);
+        t.add(headerStatusExcused);
+        t.add(headerStatusUnexcused);
+    }
+
+    private void createStatsTableData(WebMarkupContainer t) {
+        StudentDataProvider studentDataProvider = new StudentDataProvider();
+        final DataView<User> userDataView = new DataView<User>("students", studentDataProvider) {
+            @Override
+            protected void populateItem(Item<User> item) {
+                final String id = item.getModelObject().getId();
+                Map<Status, Integer> stats = attendanceLogic.getStatsForUser(id);
+                Link<Void> studentLink = new Link<Void>("student-link") {
+                    public void onClick() {
+                        setResponsePage(new StudentView(id, BasePage.STUDENT_OVERVIEW_PAGE));
+                    }
+                };
+                studentLink.add(new Label("student-name", item.getModelObject().getSortName()));
+
+                item.add(studentLink);
+                item.add(new Label("student-stats-present", stats.get(Status.PRESENT)));
+                item.add(new Label("student-stats-late", stats.get(Status.LATE)));
+                item.add(new Label("student-stats-left-early", stats.get(Status.LEFT_EARLY)));
+                item.add(new Label("student-stats-excused", stats.get(Status.EXCUSED_ABSENCE)));
+                item.add(new Label("student-stats-absent", stats.get(Status.UNEXCUSED_ABSENCE)));
+            }
+        };
+
+        Label noStudents = new Label("no-students", new ResourceModel("attendance.student.overview.no.students")) {
+            @Override
+            public boolean isVisible(){
+                return userDataView.size() <= 0;
+            }
+        };
+        Label noStudents2 = new Label("no-students2", new ResourceModel("attendance.student.overview.no.students.2")) {
+            @Override
+            public boolean isVisible(){
+                return userDataView.size() <= 0;
+            }
+        };
+
+        t.add(userDataView);
+        t.add(noStudents);
+        t.add(noStudents2);
+    }
+
 }
