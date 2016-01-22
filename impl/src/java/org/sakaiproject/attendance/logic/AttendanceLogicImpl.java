a6b1d5ab80ec66a00458280c574d1183ea4471c5
David P. Bauer <dbauer1@udayton.edu>
2016-01-22 14:39:33 -0500

    SAKAI-2369 Comments were lost when using set all to
    Need to reload the event from the database before setting all statuses on the records
    to make sure that comments entered since last load are not lost.

--- impl/src/java/org/sakaiproject/attendance/logic/AttendanceLogicImpl.java
+++ impl/src/java/org/sakaiproject/attendance/logic/AttendanceLogicImpl.java
@@ -249,9 +249,10 @@ public class AttendanceLogicImpl implements AttendanceLogic {
 	 * {@inheritDoc}
 	 */
 	public boolean updateAttendanceRecordsForEvent(AttendanceEvent aE, Status s) {
+		aE = getAttendanceEvent(aE.getId());
 		List<AttendanceRecord> records = new ArrayList<AttendanceRecord>(aE.getRecords());
 
-		if(records == null || records.isEmpty()) {
+		if(records.isEmpty()) {
 			records = generateAttendanceRecords(aE, s);
 		}
 
