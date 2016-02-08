8752a56a5d1634cc1cd2ca77115337184784b216
Leonardo Canessa <lcanessa1@udayton.edu>
2016-02-08 13:35:17 -0500

    SAKAI-2407 Revert copyright on external files.

--- tool/src/webapp/javascript/jquery.metadata.js
+++ tool/src/webapp/javascript/jquery.metadata.js
@@ -1,17 +1,14 @@
 /*
- *  Copyright (c) 2016, University of Dayton
+ * Metadata - jQuery plugin for parsing metadata from elements
  *
- *  Licensed under the Educational Community License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Copyright (c) 2006 John Resig, Yehuda Katz, J�örn Zaefferer, Paul McLanahan
  *
- *              http://opensource.org/licenses/ecl2
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Revision: $Id$
+ *
  */
 
 /**
